5fc51166bc6bd783d0824934c45d0d70f8854c35
Tyler Pooschke <tyler.pooschke@pm.me>
2022-05-10 22:34:11 -0500

    project completed

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,14 +2,18 @@ import FeedbackItem from "./FeedbackItem";
 import { motion, AnimatePresence } from "framer-motion";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-	const { feedback } = useContext(FeedbackContext);
+	const { feedback, isLoading } = useContext(FeedbackContext);
 
-	if (!feedback || feedback.length === 0) {
+	if (!isLoading && (!feedback || feedback.length === 0)) {
 		return <p>No Feedback Yet</p>;
 	}
-	return (
+
+	return isLoading ? (
+		<Spinner />
+	) : (
 		<div className="feedback-list">
 			<AnimatePresence>
 				{feedback.map((item) => (
@@ -24,6 +28,7 @@ function FeedbackList() {
 			</AnimatePresence>
 		</div>
 	);
+
 	// return (
 	// 	<div className="feedback-list">
 	// 		{feedback.map((item) => (
